9b03abf77a0d93430dbca6a15a2cc8b6d5729616
Hema Kanithi <hema.wtson@gmail.com>
2026-04-11 10:27:17 -0400

    key state inconsistancy chnaged to prod

--- terraform/environments/prod/providers.tf
+++ terraform/environments/prod/providers.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "hema-terraform-state-2026"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
